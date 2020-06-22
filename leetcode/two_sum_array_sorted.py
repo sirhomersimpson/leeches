0ff4b4a0c318263f0d773227d7fc9eb776f470ed
Rik Kisnah <rik.kisnah@oracle.com>
2020-06-21 22:24:18 -0700

    Added optimized solution for two sum sorted array problem

--- leetcode/two_sum_array_sorted.py
+++ leetcode/two_sum_array_sorted.py
@@ -15,11 +15,27 @@ class Solution:
 
         return []
 
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        #optimized solution, TC = O(N)
+        low = 0
+        high = len(numbers)-1
+        while low < high:
+            sum = numbers[low] + numbers[high]
+            if sum == target:
+                return [low, high]
+            elif sum < target:
+                low += 1
+            else:
+                high -= 1
+        return []
 
 #main
 
+
 l = [10, 23, 1, 5, 100]
 l.sort()
-print (l)
-value = Solution().twoSum_bf(l, 11)
+print(l)
+#value = Solution().twoSum_bf(l, 11)
+value = Solution().twoSum(l, 11)
+
 print(value)
