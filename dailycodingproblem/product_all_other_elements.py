88e705f58db61366116e6c44493e8e62bc8437b7
Rik <rikesh@gmail.com>
2020-10-09 19:02:53 -0700

    Added some tests

--- dailycodingproblem/product_all_other_elements.py
+++ dailycodingproblem/product_all_other_elements.py
@@ -1,20 +1,24 @@
 ## Problem 1.1 Get Product of all elements
 def products(nums):
-    prefix = []
-    for n in nums:
-        if prefix:
-            prefix.append(prefix[-1] * n)
-        else:
-            prefix.append(n)
-    print(prefix)
+    prefix = [1]
+    for index in range(1, len(nums)):
+        prod = nums[index-1] * prefix[-1]
+        prefix.append(prod)
 
-    suffix = []
-    for n in reversed(nums):
-        if suffix:
-            suffix.append(suffix[-1]*n)
-        else:
-            suffix.append(n)
+    #print(prefix)
+    suffix = [1]
+    reverse_nums = list(reversed(nums))
+    for index in range(1, len(reverse_nums)):
+        prod = reverse_nums[index-1] * suffix[-1]
+        suffix.append(prod)
     suffix = list(reversed(suffix))
-    print(suffix)
+    #print(suffix)
+
+    final_lst = []
+    for index in range(len(nums)):
+        final_lst.append(prefix[index] * suffix[index])
+
+    return final_lst
+
 
 print(products([1, 2, 3, 4, 5]))
