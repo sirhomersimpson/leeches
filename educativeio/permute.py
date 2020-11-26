e045a358cd7ee9fb858be665568850a60399ea67
Rik <rik.kisnah@oracle.com>
2020-11-26 07:15:46 -0800

    Added code for permute tests

---
+++ educativeio/permute.py
@@ -0,0 +1,61 @@
+# https://www.codeproject.com/Tips/1275693/Recursive-Permutations-in-Python
+# Recursive implementation of permutations. Time complexity is N! (But some book say it is N^2*N!
+# Note that itertools permutations is anecdotally 7 times faster than this approach
+from itertools import permutations
+import time
+import random
+import string
+
+def permute_recursive(s):
+	
+	out = []
+	# Base case:
+	if len(s) == 1:
+		return s
+	
+	for i, pref in enumerate(s):
+		# s[:i] + s[i+1:] we skip s[i] as it is the prefix
+		# e.g s="abc" - prefix=b, P(a..c)
+		for perm in permute_recursive(s[:i] + s[i+1:]):
+			out += [pref + perm]
+
+	return out
+
+
+def permute_iter(s):
+	lst = []
+	for perm in permute_recursive(s):
+		lst.append(perm)
+	return lst
+
+def load_test (k):
+	
+	#Choose a random string
+	str1 = random.choices(string.ascii_lowercase, k=k)
+	print(str1)
+	print("Starting Recursive permutation")
+	start_time = time.time()
+	#print(permute_recursive(str1))
+	permute_recursive(str1)
+	end_time = time.time()
+	diff_recur = end_time - start_time
+	print(f'(Permute Recursive) Elapsed time {diff_recur} seconds')
+	
+	print("Starting Itertools permutation")
+	start_time = time.time()
+	#print(permute_recursive(str1))
+	permute_recursive(str1)
+	end_time = time.time()
+	diff_iter = end_time - start_time
+	print(f'(Itertools Itertools) Elapsed time {diff_iter} seconds')
+
+	diff = diff_recur-diff_iter
+	diff_percent = (diff / (diff_recur)) * 100
+	print('Summary (itertools is faster than recursive by ): {0:.5f} seconds'.format(diff))
+	print('Summary (itertools is faster than recursive by ): {0:.2f} %'.format(diff_percent))
+
+
+for i in range(5, 15):
+	print(f'Load Test with load {i} running 3 times')
+	for j in range(3):
+		load_test(i)
