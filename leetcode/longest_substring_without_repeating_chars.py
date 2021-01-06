d0bc8e0c5f475cd7098ec2e35e122d41848a2a19
Rik <rik.kisnah@oracle.com>
2021-01-06 14:10:59 -0800

    Longest Substring Without Repeating Characters submission - it fails

--- leetcode/longest_substring_without_repeating_chars.py
+++ leetcode/longest_substring_without_repeating_chars.py
@@ -1,27 +1,28 @@
+"""
+https://leetcode.com/problems/longest-substring-without-repeating-characters/
+"""
 class Solution:
 
     def lengthOfLongestSubstring(self, s: str) -> int:
-
-        l = len(s)
-        ans = 0
-        for i in range(0, l):
-            print(s[:i+1])
-
-        return ans
-
-    def is_unique(self, s) -> bool:
-        char_seen = []
-        for c in s:
-            if c in char_seen:
-                return False
-            char_seen.append(c)
-        return True
-
-
-#print(Solution().lengthOfLongestSubstring('abcabcbb'))  # expect 3(abc)
-#print(Solution().lengthOfLongestSubstring('bbbbb'))  # expect 1(b)
-#print(Solution().lengthOfLongestSubstring('pwwkew'))  # expect 2
-#print(Solution().lengthOfLongestSubstring(''))  # expect 0
-
-print(Solution().lengthOfLongestSubstring('abcde'))
+        window_start = 0
+        max_length = 0
+        char_index_map = {}
+        # Step 1. Go through the windown
+        for window_end in range(len(s)):
+            right_char = s[window_end]
+            
+            # Step 2. If we see the right_char, we need to re-adjust the window
+            if right_char in char_index_map:
+                window_start = char_index_map[right_char]+1
+            
+            # Step 3. Store the window end
+            char_index_map[right_char] = window_end
+            # Step 4. Calculate the maximum
+            max_length = max(max_length, window_end-window_start+1)
+            
+        return max_length
+     
+    
+print(Solution().lengthOfLongestSubstring('aabcde')) # expects
+print(Solution().lengthOfLongestSubstring('abba'))
 
