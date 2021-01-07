725f256adaadd32c289fa6fc794f49d4b6301ad0
Rik <rik.kisnah@oracle.com>
2021-01-07 15:27:46 -0800

    Longest Substring Without Repeating Characters - fixed bug in edge case

--- leetcode/longest_substring_without_repeating_chars.py
+++ leetcode/longest_substring_without_repeating_chars.py
@@ -1,9 +1,11 @@
 """
 https://leetcode.com/problems/longest-substring-without-repeating-characters/
+Given a string s, find the length of the longest substring without repeating characters.
 """
 class Solution:
 
-    def lengthOfLongestSubstring(self, s: str) -> int:
+    @staticmethod
+    def lengthOfLongestSubstring(s: str) -> int:
         window_start = 0
         max_length = 0
         char_index_map = {}
@@ -13,7 +15,8 @@ class Solution:
             
             # Step 2. If we see the right_char, we need to re-adjust the window
             if right_char in char_index_map:
-                window_start = char_index_map[right_char]+1
+                #There is an edge case here when window_start is ahead last index of right_char
+                window_start = char_index_map[right_char] + 1
             
             # Step 3. Store the window end
             char_index_map[right_char] = window_end
@@ -23,6 +26,8 @@ class Solution:
         return max_length
      
     
-print(Solution().lengthOfLongestSubstring('aabcde')) # expects
-print(Solution().lengthOfLongestSubstring('abba'))
+#print(Solution().lengthOfLongestSubstring('aabcde')) # expects 5
+print(Solution().lengthOfLongestSubstring('abba')) # expects 2
+#print(Solution().lengthOfLongestSubstring('aa')) # expects 1
+#print(Solution().lengthOfLongestSubstring('abba')) # expects 2
 
